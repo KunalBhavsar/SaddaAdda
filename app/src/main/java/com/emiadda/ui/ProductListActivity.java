b0dd8341029e376d944558f2470138df0a5f50f5
shraddha.pednekar <spednekar71@gmail.com>
2016-07-06 21:16:50 +0530

    added loading dialog in product list

--- app/src/main/java/com/emiadda/ui/ProductListActivity.java
+++ app/src/main/java/com/emiadda/ui/ProductListActivity.java
@@ -1,5 +1,7 @@
 package com.emiadda.ui;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,13 +11,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.emiadda.R;
+import com.emiadda.interafaces.ServerResponseInterface;
 
-public class ProductListActivity extends AppCompatActivity {
+public class ProductListActivity extends AppCompatActivity implements ServerResponseInterface {
 
     private RecyclerView.LayoutManager layoutManager;
     private RecyclerView recyclerView;
 //    private ProductGridAdapter productGridAdapter;
     private Toolbar toolbar;
+    private ProgressDialog progressDialog;
+    private Activity mActivityContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,11 @@ public class ProductListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading..");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.addItemDecoration(new GridSpaceItemDecoration(10));
@@ -33,6 +43,11 @@ public class ProductListActivity extends AppCompatActivity {
         layoutManager = new GridLayoutManager(ProductListActivity.this, 2);
         recyclerView.setLayoutManager(layoutManager);
   //      recyclerView.setAdapter(productGridAdapter);
+    }
+
+    @Override
+    public void responseReceived(String response, int requestCode, int responseCode) {
+        progressDialog.dismiss();
     }
 
     public class GridSpaceItemDecoration extends RecyclerView.ItemDecoration {
